f6e22835b5c192e375bbab4b2f39c2fca8cb1aa0
liuqian1218 <2363600612@qq.com>
2021-06-15 09:47:56 +0800

    2021-06-15

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>主页</div>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"Home",
+  name: 'Home',
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -26,4 +26,16 @@ const router = new VueRouter({
   routes
 })
 
+//挂载导航守卫
+router.beforeEach((to,from,next) => {
+  if(to.path === "/login"){
+    return next();
+  }
+  const tokenStr = window.sessionStorage.getItem("token")
+  if(!tokenStr){
+    return next("/login")
+  }
+  next();
+})
+
 export default router
